cd27d3fa047d45fb69ba5d37730517b5b7c3a08f
Rod Glover <rglover@uvic.ca>
2017-03-03 16:21:59 -0800

    Update Development Status classifier

--- setup.py
+++ setup.py
@@ -17,15 +17,15 @@ setup(
     install_requires = ['netCDF4'],
     package_data = {'nchelpers': ['data/tiny_gcm.nc']},
     include_package_data = True,
-    classifiers=['Development Status :: 5 - Production/Stable',
+    classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Console',
                  'Intended Audience :: Developers',
                  'Intended Audience :: Science/Research',
                  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.3',
-                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
                  'Topic :: Scientific/Engineering',
                  'Topic :: Database',
                  'Topic :: Software Development :: Libraries :: Python Modules'
