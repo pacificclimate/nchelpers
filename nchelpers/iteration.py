4720d1ac3c350b8c5e1ad071b8739b7fe0bfeea9
Rod Glover <rglover@uvic.ca>
2017-11-28 17:47:49 -0800

    Add array chunking utilties

---
+++ nchelpers/iteration.py
@@ -0,0 +1,75 @@
+"""
+Utilities for chunking arbitrary arrays.
+
+Definition of terms used:
+
+An array is a multidimensional array of shape ``shape`` and dimension
+``n`` = ``len(shape)``. Think ``numpy``.
+
+An index is a ``n``-tuple specifying a single element in the
+multidimensional array. Indexing on each dimension is zero-based.
+
+A chunk is a contiguous rectangular subset of the array with maximum extent
+(length) in each coordinate specified by ``chunk_shape``, but limited by
+the bounds of the array. (Chunks near the outside edges of the array are
+truncated if a whole number of chunks does not fit on each dimension of
+the array).
+
+A corner is the index within a chunk that has the smallest values in
+each coordinate component.
+"""
+
+def chunk_corners(shape, chunk_shape, d=0):
+    """
+    Generator that enumerates the index of the corner of each chunk of
+    a specified chunk shape within a specified array shape.
+
+    Currently, there is only one ordering available: the first index varies
+    fastest, also known as C ordering.
+
+    :param shape: (tuple) shape of array
+    :param chunk_shape: (tuple) shape of (full-size) chunk
+    :param d: (int) return coordinates for subset of array defined by
+        coordinates indexed ``d``, ``d+1``, ..., ``n-1``.
+    :return (generator) that yields indices of chunk corners.
+    """
+    assert len(shape) == len(chunk_shape)
+    if d >= len(shape):
+        yield ()
+    else:
+        axis = range(0, shape[d], chunk_shape[d])
+        for index in axis:
+            for indices in chunk_corners(shape, chunk_shape, d+1):
+                yield (index,) + indices
+
+
+def chunk_slices(shape, chunk_shape):
+    """
+    Generator that enumerates indexing tuples containing slices that pick
+    out the chunks of shape ``chunk_shape`` from an array of shape ``shape``.
+
+    :param shape: (tuple) array shape
+    :param chunk_shape: (tuple) shape of (full-size) chunk
+    :return: (generator) that yields ``n``-tuples of the form
+            ``(s[0], s[1], ..., s[n-1])``
+        where each ``s[d]`` is a slice object valid for dimension ``d`` of
+        the array. Taken together, the slices exhaust all indices of the array.
+    """
+    for index in chunk_corners(shape, chunk_shape):
+        yield tuple(
+            slice(i, min(i+c, s))
+            for i, s, c in zip(index, shape, chunk_shape)
+        )
+
+def chunks(array, chunk_shape):
+    """
+    Generator that returns all chunks of shape ``chunk_shape`` in
+    array ``array``.
+
+    :param array: (numpy.array) array to be chunked
+    :param chunk_shape: (tuple) shape of (full-size) chunk
+    :return: (generator) that enumerates all chunks in array; a chunk
+        is a numpy array
+    """
+    for chunk_slice in chunk_slices(array.shape, chunk_shape):
+        yield array[chunk_slice]
---
+++ tests/test_iteration.py
@@ -0,0 +1,88 @@
+from math import ceil
+from operator import mul
+from functools import reduce
+from itertools import product
+
+import pytest
+from nchelpers.iteration import chunk_corners, chunk_slices, chunks
+
+import numpy as np
+
+
+def size(shape):
+    return reduce(mul, shape, 1)
+
+
+def num_chunks(shape, chunk_shape):
+    """Number of chunks of shape ``chunk_shape`` in an array of shape ``shape``."""
+    return reduce(mul, (ceil(s/c) for s, c in zip(shape, chunk_shape)), 1)
+
+
+def union(sets):
+    """Union of all sets in iterator ``sets``."""
+    result = set()
+    for s in sets:
+        result = result | s
+    return result
+
+
+def indices(slice, shape):
+    """Generator yielding all indices specified by a slice tuple
+    constrained by an array of shape ``shape``."""
+    return product(*(range(*sl.indices(sh)) for sl, sh in zip(slice, shape)))
+
+
+sc = [
+    ((2, 3), (1, 1)),
+    ((2, 3, 2), (1, 1, 1)),
+    ((3, 3), (1, 2)),
+    ((3, 3), (2, 2)),
+    ((10, 10, 10), (3, 5, 7)),
+]
+
+@pytest.mark.parametrize('shape, chunk_shape', sc)
+def test_print_chunk_indices(shape, chunk_shape):
+    print()
+    for index in chunk_corners(shape, chunk_shape):
+        print(index)
+
+
+@pytest.mark.parametrize('shape, chunk_shape', sc)
+def test_chunk_indices(shape, chunk_shape):
+    corners = list(chunk_corners(shape, chunk_shape))
+    # One corner per chunk
+    assert len(corners) == num_chunks(shape, chunk_shape)
+    # Along each dimension, the corner indices are exactly those specified
+    # by the shape of the array and stride from the chunk shape
+    for d, (s, c) in enumerate(zip(shape, chunk_shape)):
+        assert {index[d] for index in corners} == set(range(0, s, c))
+
+
+@pytest.mark.parametrize('shape, chunk_shape', sc)
+def test_print_chunk_slices(shape, chunk_shape):
+    print()
+    for chunk_slice in chunk_slices(shape, chunk_shape):
+        print(chunk_slice)
+
+
+@pytest.mark.parametrize('shape, chunk_shape', sc)
+def test_chunk_slices(shape, chunk_shape):
+    slices = list(chunk_slices(shape, chunk_shape))
+    # One slice per chunk
+    assert len(slices) == num_chunks(shape, chunk_shape)
+    # The union of the sets of indices specified by the slices (set per slice)
+    # exhausts all indices in the array.
+    assert union(set(indices(slice, shape)) for slice in slices) == \
+           set(product(*(range(s) for s in shape)))
+
+
+@pytest.mark.parametrize('array, chunk_shape', [
+    (np.arange(size(shape)).reshape(shape), chunk_shape)
+    for shape, chunk_shape in sc
+])
+def test_print_chunks(array, chunk_shape):
+    print()
+    for chunk in chunks(array, chunk_shape):
+        print(chunk)
+
+
